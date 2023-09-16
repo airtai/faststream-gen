0f0ee677107c008f4f1f78e25254f58bf4a2962f
“Robert <robert@airt.ai>
2023-09-17 00:35:55 +0200

    Faststream Cryptocurreny tutorial finished

--- docs_src/tutorial/calculate_mean_app.py
+++ docs_src/tutorial/calculate_mean_app.py
@@ -1,45 +1,50 @@
 from typing import Dict, List
 
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, NonNegativeFloat
 
 from faststream import Context, ContextRepo, FastStream, Logger
 from faststream.kafka import KafkaBroker
 
-class NewData(BaseModel):
-    price: float = Field(
-        ..., examples=[1000], description="Current price of the crypto"
-    )
-    currency: str = Field(
-        ..., examples=["BTC"], description="Currency of the price"
-    )
-
 broker = KafkaBroker("localhost:9092")
 app = FastStream(broker)
 
-to_price_mean = broker.publisher("price_mean")
+
+class CryptoPrice(BaseModel):
+    price: NonNegativeFloat = Field(
+        ..., examples=[50000], description="Current price of the cryptocurrency"
+    )
+    crypto_currency: str = Field(
+        ..., examples=["BTC"], description="Cryptocurrency symbol (e.g. BTC, ETH)"
+    )
+
+
+publisher = broker.publisher("price_mean")
+
 
 @app.on_startup
 async def app_setup(context: ContextRepo):
     message_history: Dict[str, List[float]] = {}
     context.set_global("message_history", message_history)
 
+
 @broker.subscriber("new_data")
 async def on_new_data(
-    msg: NewData,
+    msg: CryptoPrice,
     logger: Logger,
-    context: ContextRepo,
+    message_history: Dict[str, List[float]] = Context(),
     key: bytes = Context("message.raw_message.key"),
 ) -> None:
-    logger.info(f"{msg=}")
-
-    message_history = context.get("message_history")
+    logger.info(f"New data received: {msg=}")
 
     partition_key = key.decode("utf-8")
     if partition_key not in message_history:
         message_history[partition_key] = []
 
     message_history[partition_key].append(msg.price)
-    context.set_global("message_history", message_history)
 
-    price_mean = sum(message_history[partition_key][-3:]) / 3
-    await to_price_mean.publish(price_mean, key=key)
+    if len(message_history[partition_key]) > 100:
+        message_history[partition_key].pop(0)
+
+    if len(message_history[partition_key]) >= 3:
+        price_mean = sum(message_history[partition_key][-3:]) / 3
+        await publisher.publish(price_mean, key=key)
--- docs_src/tutorial/scrape_and_publish_app.py
+++ docs_src/tutorial/scrape_and_publish_app.py
@@ -1,38 +1,24 @@
-from pydantic import BaseModel, Field, NonNegativeFloat
+import asyncio
+import json
 
+from pydantic import BaseModel, Field, NonNegativeFloat
 from faststream import ContextRepo, FastStream, Logger
 from faststream.kafka import KafkaBroker
-
 import requests
-import asyncio
-
-class CryptoPrice(BaseModel):
-    price: NonNegativeFloat = Field(..., description="Current price of the cryptocurrency")
-    currency: str = Field(..., description="Currency of the price")
 
 broker = KafkaBroker("localhost:9092")
 app = FastStream(broker)
 
-new_data_publisher = broker.publisher("new_data")
+publisher = broker.publisher("new_data")
 
-async def fetch_and_publish_crypto_price(
-    url: str,
-    key: str,
-    logger: Logger,
-    context: ContextRepo,
-    time_interval: int = 2,
-) -> None:
-    while context.get("app_is_running"):
-        response = requests.get(url)
-        if response.status_code == 200:
-            data = response.json()
-            price = data["data"]["amount"]
-            currency = data["data"]["base"]
-            crypto_price = CryptoPrice(price=price, currency=currency)
-            await new_data_publisher.publish(crypto_price, key=key.encode("utf-8"))
-        else:
-            logger.warning(f"Failed to fetch data from {url}")
-        await asyncio.sleep(time_interval)
+
+class CryptoPrice(BaseModel):
+    price: NonNegativeFloat = Field(
+        ..., examples=[50000.0], description="Current price of cryptocurrency in USD"
+    )
+    crypto_currency: str = Field(
+        ..., examples=["BTC"], description="Cryptocurrency symbol e.g BTC, ETH..."
+    )
 
 
 @app.on_startup
@@ -44,20 +30,54 @@ async def app_setup(context: ContextRepo):
 async def app_shutdown(context: ContextRepo):
     context.set_global("app_is_running", False)
 
+    # Get the running task and await for it to finish
+    publish_tasks = context.get("publish_tasks")
+    await asyncio.wait(publish_tasks)
+
+
+async def fetch_and_publish_crypto_price(
+    crypto_currency: str,
+    logger: Logger,
+    context: ContextRepo,
+    time_interval: int = 2,
+) -> None:
+    # Always use context: ContextRepo for storing app_is_running variable
+    while context.get("app_is_running"):
+        if crypto_currency == "BTC":
+            url = "https://api.coinbase.com/v2/prices/BTC-USD/spot"
+        elif crypto_currency == "ETH":
+            url = "https://api.coinbase.com/v2/prices/ETH-USD/spot"
+        else:
+            logger.warning(f"Invalid crypto currency: {crypto_currency}")
+            continue
+
+        response = requests.get(url)
+
+        if response.status_code == 200:
+            # read json response
+            raw_data = json.loads(response.content)
+            price = raw_data["data"]["amount"]
+
+            new_data = CryptoPrice(price=price, crypto_currency=crypto_currency)
+            key = crypto_currency.encode("utf-8")
+            await publisher.publish(new_data, key=key)
+        else:
+            logger.warning(f"Failed API request {url}")
+
+        await asyncio.sleep(time_interval)
+
 
 @app.after_startup
-async def publish_crypto_prices(logger: Logger, context: ContextRepo):
+async def publish_crypto_price(logger: Logger, context: ContextRepo):
     logger.info("Starting publishing:")
 
-    bitcoin_url = "https://api.coinbase.com/v2/prices/BTC-USD/spot"
-    ethereum_url = "https://api.coinbase.com/v2/prices/ETH-USD/spot"
-
+    crypto_currencies = ["BTC", "ETH"]
+    # start fetching and publishing crypto prices
     publish_tasks = [
         asyncio.create_task(
-            fetch_and_publish_crypto_price(bitcoin_url, "BTC", logger, context)
-        ),
-        asyncio.create_task(
-            fetch_and_publish_crypto_price(ethereum_url, "ETH", logger, context)
-        ),
+            fetch_and_publish_crypto_price(crypto_currency, logger, context)
+        )
+        for crypto_currency in crypto_currencies
     ]
+    # you need to save asyncio tasks so you can wait for them to finish at app shutdown (the function with @app.on_shutdown function)
     context.set_global("publish_tasks", publish_tasks)
