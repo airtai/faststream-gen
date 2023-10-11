9eee1c839d69194475c3d296eaf97fb1cf599ff5
harishmohanraj <harish@airt.ai>
2023-10-11 00:14:19 +0000

    Add test files for examples

---
+++ search/tests/examples/test_example_forward_to_multiple_topics.py
@@ -0,0 +1,3 @@
+from search.examples.example_forward_to_multiple_topics.test_app import test_app
+
+__all__ = ("test_app",)
---
+++ search/tests/examples/test_example_infinity_publishing.py
@@ -0,0 +1,7 @@
+# import pytest
+
+from search.examples.example_infinity_publishing.test_app import (
+    test_message_was_published,
+)
+__all__ = ("test_message_was_published",)
+# pytest.mark.slow(test_message_was_published)
